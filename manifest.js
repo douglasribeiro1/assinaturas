36f01359e0bc749e18701d10b3042ac025f411f3
douglasribeiro1 <140409212+douglasribeiro1@users.noreply.github.com>
2026-06-01 12:18:29 -0300

    Change icon source to inline SVG format
    Updated icon source to use an inline SVG and added purpose.

--- manifest.js
+++ manifest.js
@@ -7,9 +7,10 @@
   "theme_color": "#4A90E2",
   "icons": [
     {
-      "src": "https://flaticon.com",
+      "src": "data:image/svg+xml,<svg xmlns=%22http://w3.org viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💳</text></svg>",
       "sizes": "512x512",
-      "type": "image/png"
+      "type": "image/svg+xml",
+      "purpose": "any maskable"
     }
   ]
 }
